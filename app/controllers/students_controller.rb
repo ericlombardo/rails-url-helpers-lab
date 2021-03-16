74ca300a2da7bc3c63ea44c42bc503139ebd79a7
Eric <eric.m.lombardo@gmail.com>
2021-03-16 14:29:25 -0400

    commit tree 49c576be64bad68366e50d2457609c98f47eebd2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,14 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @active_response = @student.active ? ("This student is currently active.") : ("This student is currently inactive.")
+  end
+
+  def active
+    student = Student.find_by_id(params[:id])
+    student.toggle!(:active)
+    # byebug
+    redirect_to student_path(student)
   end
 
   private
